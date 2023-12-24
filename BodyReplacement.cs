4b5726795d7154908b41b6f31bffba1e3e0a7b50
NotAustinVT <154647175+NotAustinVT@users.noreply.github.com>
2023-12-23 20:23:39 -0500

    Add files via upload

---
+++ BodyReplacement.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Zeekerss;
+using Zeekerss.Core;
+using Zeekerss.Core.Singletons;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Xml;
+using System.IO;
+using BepInEx;
+using GameNetcodeStuff;
+using UnityEngine.PlayerLoop;
+using Unity.Netcode;
+using System.Reflection;
+using HarmonyLib;
+using ModelReplacement;
+
+namespace PlayerAvatarLoader
+{
+    public class BodyReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            PlayerControllerB component = ((Component)this).gameObject.GetComponent<PlayerControllerB>();
+            ulong playerSteamId = component.playerSteamId;
+            string playerSteamName = component.playerUsername;
+            int suitId = component.currentSuitID;
+            string model_name = StartOfRound.Instance.unlockablesList.unlockables[suitId].unlockableName;
+
+            if (model_name == "Default")
+            {
+                Debug.LogWarning("Detected An Attempt To Load Suit 'Default'. This Is Likely Because MoreSuits Is Installed. Falling Back To 'Orange suit'.");
+                model_name = "Orange suit";
+            }
+            
+            Debug.LogWarning("Attempting To Load '" + model_name + "' For " + playerSteamName + "(" + playerSteamId.ToString() + ")");
+
+            if (Assets.loadedBundleIds.FirstOrDefault(stringToCheck => stringToCheck.Contains(playerSteamId.ToString())) != null)
+            {
+                Debug.LogWarning("Found Previously Loaded Bundle For " + playerSteamName + "(" + playerSteamId.ToString() + ")");
+
+                int j = Assets.loadedBundleIds.FindIndex(x => x.Contains(playerSteamId.ToString()));
+                GameObject modelPre = Assets.loadedAssetBundles[j].LoadAsset<GameObject>(model_name);
+
+                if (modelPre != null)
+                {
+                    return modelPre;
+                }
+                else
+                {
+                    Debug.LogError("No '" + model_name + "' Model Found For " + playerSteamId.ToString() + "!!! Falling Back To Default");
+                    modelPre = Assets.initAssetBundle.LoadAsset<GameObject>(model_name);
+
+                    if (modelPre == null)
+                    {
+                        Debug.LogError("No '" + model_name + "' Model Found Inside Default Bundle!!! Falling Back To 'Orange suit'");
+                        return Assets.initAssetBundle.LoadAsset<GameObject>("Orange suit");
+                    }
+
+                    return modelPre;
+                }
+
+            }
+            else
+            {
+                Debug.LogWarning("Bundle Not Yet Loaded For " + playerSteamName + "(" + playerSteamId.ToString() + ")" + ". Attempting To Load New Bundle...");
+                AssetBundle userAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Avatars", playerSteamId.ToString()));
+
+                if (userAssetBundle != null)
+                {
+                    Assets.PopulateAssets(true, playerSteamId.ToString(), userAssetBundle);
+                    GameObject modelPre = userAssetBundle.LoadAsset<GameObject>(model_name);
+
+                    if (modelPre != null)
+                    {
+                        return modelPre;
+                    }
+                    else
+                    {
+                        Debug.LogError("No '" + model_name + "' Model Found For " + playerSteamId.ToString() + "!!! Falling Back To Default");
+                        return Assets.initAssetBundle.LoadAsset<GameObject>(model_name);
+                    }
+                }
+                else
+                {
+                    Debug.LogError(playerSteamName + "(" + playerSteamId.ToString() + ")" + " Does Not Have A Bundle!!! Falling Back To Default.");
+                    return Assets.initAssetBundle.LoadAsset<GameObject>(model_name);
+                }
+                
+            }
+        }
+
+        protected override void AddModelScripts()
+        {
+            //Bruh how the fuck do you read a json?
+
+            /*PlayerControllerB component = ((Component)this).gameObject.GetComponent<PlayerControllerB>();
+            ulong playerSteamId = component.playerSteamId;
+            string playerSteamName = component.playerUsername;
+            int suitId = component.currentSuitID;
+            string model_name = StartOfRound.Instance.unlockablesList.unlockables[suitId].unlockableName;
+            if (model_name == "Default") { model_name = "Orange suit"; }
+
+            string jsonName = playerSteamId.ToString() + ".json";
+
+            if (File.Exists(Path.Combine(Paths.GameRootPath, "Avatars", jsonName)))
+            {
+                Debug.LogWarning("Dynamic Bones Found For " + playerSteamName + "(" + playerSteamId.ToString() + "). Checking Settings For '" + model_name + "'...");
+                string jsonText = File.ReadAllText(Path.Combine(Paths.GameRootPath, "Avatars", jsonName));
+                var jsonData = JsonConvert.DeserializeObject<DynBones>(jsonText);
+                Debug.LogWarning(jsonData.Suits.ToString());
+                Debug.LogWarning(jsonData.SuitData.ToString());
+            }
+            else
+            {
+                Debug.LogError("No Dynamic Bones Found For " + playerSteamName + "(" + playerSteamId.ToString() + "). Skipping Step.");
+            }*/
+        }
+
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,106 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Zeekerss;
+using Zeekerss.Core;
+using Zeekerss.Core.Singletons;
+using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Reflection;
+using GameNetcodeStuff;
+using ModelReplacement;
+
+//using System.Numerics;
+
+namespace PlayerAvatarLoader
+{
+
+
+
+    [BepInPlugin("NotAustinVT.PlayerAvatarLoader", "Player Avatar Loader", "1.0")]
+    [BepInDependency("meow.ModelReplacementAPI", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Plugin.SuitsToReg = base.Config.Bind<string>("Suits - ONLY EDIT FOR SUIT MOD COMPATIBILITY", "Suits", "Orange suit,Green suit,Hazard suit,Pajama suit", "Include all suits being loaded. (If using More_Suits, DO NOT INCLUDE 'Default' IN THE LIST OF SUITS TO LOAD");
+            char[] commaSplit = new char[] { ',' };
+            string[] suitRegistry = SuitsToReg.Value.Split(commaSplit);
+
+            ModelReplacementAPI.RegisterSuitModelReplacement("Default", typeof(BodyReplacement));
+            for ( int i = 0; i < suitRegistry.Length; i++)
+            {
+                if (suitRegistry[i] != "Default") { ModelReplacementAPI.RegisterSuitModelReplacement(suitRegistry[i], typeof(BodyReplacement)); }
+            }
+
+            //ModelReplacementAPI.RegisterSuitModelReplacement("Green suit", typeof(BodyReplacement));
+            //ModelReplacementAPI.RegisterSuitModelReplacement("Orange suit", typeof(BodyReplacement));
+            //ModelReplacementAPI.RegisterSuitModelReplacement("Pajama suit", typeof(BodyReplacement));
+            //ModelReplacementAPI.RegisterSuitModelReplacement("Hazard suit", typeof(BodyReplacement));
+
+            Assets.PopulateAssets(false, "if you see this you're gay", null);
+
+            Harmony harmony = new Harmony("NotAustinVT.AvatarLoader");
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {"NotAustinVT.PlayerAvatarLoader"} is loaded!");
+        }
+
+
+
+        [HarmonyPatch(typeof(PlayerControllerB))]
+        public class PlayerControllerBPatch
+        {
+
+            [HarmonyPatch("Update")]
+            [HarmonyPostfix]
+            public static void UpdatePatch(ref PlayerControllerB __instance)
+            {
+                if (__instance.playerSteamId == 0) { return; }
+
+            }
+
+        }
+
+        public static readonly Harmony Harmony = new Harmony("NotAustinVT.PlayerAvatarLoader");
+        public static ConfigEntry<string> SuitsToReg = null;
+
+    }
+    public static class Assets
+    {
+        public static AssetBundle initAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Avatars", "default"));
+
+        public static List<string> loadedBundleIds = new List<string>();
+        public static List<AssetBundle> loadedAssetBundles = new List<AssetBundle>();
+
+        private static string GetAssemblyName() => Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+        public static void PopulateAssets(bool check, string playerSteamId, AssetBundle playerAssetBundle)
+        {
+            if (check)
+            {
+                loadedBundleIds.Add(playerSteamId);
+                loadedAssetBundles.Add(playerAssetBundle);
+
+            }
+        }
+
+    }
+
+    public class DynBones
+    {
+        /*
+        //All Referenced Suits
+        public List<string> Suits { get; set; }
+
+        //Motion Data
+        public List<string> SuitData { get; set; }
+
+        //Collider Data
+        */
+    }
+}
